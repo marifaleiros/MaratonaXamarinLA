300136ffeea6499c1bdf0bf4e29685ca0c3c5d5e
Mariana <marifaleiros@hotmail.com>
2017-02-07 20:50:52 -0200

    Lab1

---
+++ Maratona.Labs.Lab1/Maratona.Labs.Lab1.Android/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Maratona.Labs.Lab1.Android
+{
+    [Activity(Label = "Maratona.Labs.Lab1.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var button = FindViewById<Button>(Resource.Id.button1);
+            button.Click += delegate
+            {
+                button.Text = string.Format("{0} clicks", count.ToString());
+                count++;
+            };
+        }
+    }
+}
+
